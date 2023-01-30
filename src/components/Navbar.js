c24b95085f1953624602d100fb2117dc443d7a6f
apple <apple@apples-MacBook-Air.local>
2023-01-30 19:43:43 +0530

    Second Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = () => {
       <ul className="nav-icons">
         {socialLinks.map((link) => {
           return (<li key={link.id}> 
-          <a href={link.href} target="_blank" className="nav-icon"
+          <a href={link.href} target="_blank" rel="noreferrer" className="nav-icon"
             ><i className={link.icon}></i></a>
         </li>)})}
       </ul>
